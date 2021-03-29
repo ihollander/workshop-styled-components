0f08fe56aacc750d42c91fcbccc770d11a4b66ec
ihollander <ianholla@gmail.com>
2021-03-29 11:58:22 -0400

    Changed material-ui to bootstrap

--- src/solution/02.js
+++ src/solution/02.js
@@ -1,10 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import styled from "styled-components";
-import { Button } from "@material-ui/core";
+import Button from "react-bootstrap/Button";
 
 const StyledButton = styled(Button)`
-  width: 200px;
+  padding: 8px 16px;
   box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  padding: 7px 14px;
+  background-color: #6772e5;
+  border: 1px solid #6772e5;
+
+  &:hover {
+    background-color: #5469d4;
+  }
 `;
 
 export default function App() {
