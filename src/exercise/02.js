3970009613224f4df803505ac0a8b95464a2ad38
ihollander <ianholla@gmail.com>
2021-04-19 08:22:44 -0400

    Fixed exercise 2 comment

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -6,14 +6,14 @@ import Button from "react-bootstrap/Button";
 // ✅ create a StyledButton component by styling the Button from react-bootstrap
 /*
 ✅ here are the new button styles:
-padding: 8px 16px
+padding: 8px 16px;
 box-shadow:
-  0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08)
-background-color: #6772e5
-border: 1px solid #6772e5
+  0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+background-color: #6772e5;
+border: 1px solid #6772e5;
 
 ✅ here is the hover style:
-background-color: #5469d4
+background-color: #5469d4;
 */
 
 export default function App() {
